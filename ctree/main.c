aed026d8d474611f23c0d4e0af3f658da5552c4d
Attila Kovacs <kovacs.a2@gmail.com>
2015-12-12 14:26:53 +0100

    Initial commit.

---
+++ ctree/main.c
@@ -0,0 +1,24 @@
+/*
+ * ctree.c
+ *
+ * Created: 2015.12.12. 14:20:29
+ * Author : Csatti
+ */ 
+
+#include <avr/io.h>
+
+
+void setup(void)
+{
+	
+}
+
+int main(void)
+{
+    /* Replace with your application code */
+	setup();
+    while (1) 
+    {
+    }
+}
+
